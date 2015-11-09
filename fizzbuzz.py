377ad262eba306e04b549724031c00a163b8a192
chezbasson <chezbasson@hotmail.com>
2015-11-09 17:29:32 +0000

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+maxNumber = int(raw_input("Maximum number:"))
+for i in range(1, maxNumber+1):
+	if(i % 3 == 0 and i % 5 == 0):
+		print("FizzBuzz")
+	elif(i % 3 == 0):
+		print("Fizz")
+	elif(i % 5 == 0):
+		print("Buzz")
+	else:
+		print(i)
